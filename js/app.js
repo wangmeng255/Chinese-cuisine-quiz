98ae5ca3159b712a735f0adf28d307904797f814
wangmeng <wangmeng255@gmail.com>
2016-06-01 22:11:40 -0700

    active page button

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
 	var answer = 0;
 	var correct = 0;
-	$("#salty").click( function() {
-		console.log("salty click");
+	$("#salty").hover( function() {
+		$("#Shan_Dong").css("opacity", "0.75");
+	}, function () {
+		$("#Shan_Dong").css("opacity", "1");
+	})
+	.click( function() {
 		$(".map").hide();
 		$(".saltyQuestion").show();
+		$(this).css({"opacity": "0.6", "pointer-events": "none"});
+		$("#lu circle").css("fill", "#9FA8DA");
+	});
+	$(".saltyQuestion .sub").click (function (event) {
+		event.preventDefault();
+		answer += 1;
+		console.log("sub clicked");
 	});
 });
